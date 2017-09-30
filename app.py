0003ce0bfdcf7c4ec91ea04bed1ce69ff23c48c3
isna <isna@sebangsa,com>
2017-09-30 11:40:17 +0700

    Some improvement on displaying the result and make a function to be reused

--- app.py
+++ app.py
@@ -1,56 +1,52 @@
 import sys
 import feedparser
 
-
 from colorama import init
 from colorama import Back, Style
+
 init(autoreset=True)
 
-# print(Fore.RED + 'some red text')
-# print('automatically back to default color again')
+def rss_print(title, link):
+    print(Back.RED + "Title:" + Style.RESET_ALL + " " + title)
+    print(Back.YELLOW + "Link :" + Style.RESET_ALL + " " + link)
 
-url = sys.argv[1]
+def get_rss(limit):
+    """
+    Get rss data from url,
+    print single entry to show latest news, else give index number
+    """
+    rss_data = feedparser.parse(URL)
+    if limit ==1:
+        title = rss_data.entries[0].title
+        link = rss_data.entries[0].link
+        rss_print(title, link)
+    else:
+        for i in range(0, limit):
+            title = rss_data.entries[i].title
+            link = rss_data.entries[i].link
 
-
-def latest():
-    d = feedparser.parse(url)
-    print (Back.RED + "Title:") + (Style.RESET_ALL + " " + d.entries[0].title)
-    print (Back.YELLOW + "Link:") + (Style.RESET_ALL + " " + d.entries[0].link)
-
-
-def latest_five():
-    d = feedparser.parse(url)
-    i = 0
-    for i in range(0, 5):
-        print (Back.CYAN + "\t" + str(i + 1) + "\t")
-        print (Back.RED + "Title:") + \
-            (Style.RESET_ALL + " " + d.entries[i].title)
-        print (Back.YELLOW + "Link:") + \
-            (Style.RESET_ALL + " " + d.entries[i].link)
-        # print "Content: " + d.entries[i]['content']
-
-
-def show(title, link, desc):
-    print str(title)
-    # print str(date)
-    print str(link)
-    print str(desc)
-
+            print(Back.CYAN + str(i + 1) + "\t")
+            rss_print(title, link)
 
 def menu():
-    print """What do you wish to do now? \n1. Read the latest issue.
-          \n2. Get the titles of the latest 5 issues."""
-    opt = int(raw_input('opt: '))
+    print("""What do you wish to do now? \n1. Get the latest issue.
+          \n2. Get the titles of the latest 5 issues.""")
+    try:
+        opt = int(raw_input('opt: '))
+    except:
+        opt = int(input('opt: '))
+
     if opt == 1:
-        latest()
+        get_rss(1)
     elif opt == 2:
-        latest_five()
+        get_rss(5)
     else:
-        print "Not a valid choice"
+        print("Not a valid choice")
         exit(0)
 
-
 if __name__ == "__main__":
-    # parse(sys.argv[1])
-    url = sys.argv[1]
-    menu()
+    URL = sys.argv[1]
+    if URL is None:
+        print("Please specify some rss link")
+    else:
+        menu()
