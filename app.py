2f8a7e02ad8517c6c8d090b9a4235f8eb5d2d0ed
Matheus Cunha Reis <matheuscunhareis30@gmail.com>
2017-10-01 02:25:18 -0300

    Add Date and Description

--- app.py
+++ app.py
@@ -8,12 +8,15 @@ from colorama import Back, Style, Fore
 init(autoreset=True)
 
 
-def rss_print(title, link):
+def rss_print(title, link, date, description, option):
     print(Back.RED + "Title:" + Style.RESET_ALL + " " + title)
+    print(Back.YELLOW + "Date :" + Style.RESET_ALL + " " + date)
     print(Back.YELLOW + "Link :" + Style.RESET_ALL + " " + link)
+    if option == 1:
+        print(Back.YELLOW + "Description :" + Style.RESET_ALL + " " + description)
 
 
-def get_rss(limit):
+def get_rss(limit, option):
     """
     Get rss data from url,
     print single entry to show latest news, else give index number
@@ -23,10 +26,12 @@ def get_rss(limit):
         for i in range(0, limit):
             title = rss_data.entries[i].title
             link = rss_data.entries[i].link
+            date = rss_data.entries[i].published if 'published' in rss_data.entries[i] else ""
+            description = rss_data.entries[i].description if 'description' in rss_data.entries[i] else ""
             print(Back.CYAN + str(i + 1) + "\t")
-            rss_print(title, link)
+            rss_print(title, link, date, description, option)
     except:
-        print(Fore.RED + """Either the feed is invalid, or 
+        print(Fore.RED + """Either the feed is invalid, or
             the feed doesn't contain many titles.""")
 
 
@@ -35,9 +40,9 @@ def menu():
           \n2. Get the titles of the latest 5 issues.""")
     opt = int(raw_input('Option: '))
     if opt == 1:
-        get_rss(1)
+        get_rss(1, 1)
     elif opt == 2:
-        get_rss(5)
+        get_rss(5, 2)
     else:
         print("Not a valid choice")
         exit(0)
